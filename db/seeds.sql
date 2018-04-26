cbf801def2af42f302ea284f2b5e40bd6b1ba3d8
Dorian Crawford <doriancrawford@Dorians-MacBook-Pro.local>
2018-04-26 09:28:24 -0400

    push app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Burger');
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (col, val, cb) {
+        orm.insertOne("burgers", col, val, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (col, val, id, cb) {
+        orm.updateOne("burgers", col, val, id, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
